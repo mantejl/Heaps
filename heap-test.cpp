0e9517d2599900bad64c7fd2048b1735cceff159
Mantej Lamba <mlamba@usc.edu>
2022-10-17 21:49:32 +0000

    testing

--- heap-test.cpp
+++ heap-test.cpp
@@ -5,26 +5,50 @@
 using namespace std;  
 
 int main() {
-    Heap <int> heap; 
-    heap.push(9); 
-    heap.push(1); 
-    heap.push(2); 
-    heap.push(6); 
-    heap.push(83); 
-    heap.push(12); 
-    heap.push(11); 
-    heap.push(5); 
-    heap.push(13); 
+    Heap <int> heap (4); 
+    // heap.push(9); 
+    // heap.push(1); 
+    // heap.push(2); 
+    // heap.push(6); 
+    // heap.push(83); 
+    // heap.push(12); 
+    // heap.push(11); 
+    // heap.push(5); 
+    // heap.push(13); 
+    // heap.push(19); 
+    // heap.push(0); 
+    // cout << heap.top() << endl; 
+    // heap.pop();
+    // cout << heap.top() << endl; 
+    // heap.pop();
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    // heap.pop(); 
+    // cout << heap.top() << endl; 
+    heap.push(65); 
+    heap.push(25); 
+    heap.push(37); 
     heap.push(19); 
-    heap.push(0); 
-    cout << heap.top() << endl; 
-    heap.pop();
-    cout << heap.top() << endl; 
-    heap.pop();
-    cout << heap.top() << endl; 
-    heap.pop(); 
-    cout << heap.top() << endl; 
-    heap.pop(); 
+    heap.push(3);
+    heap.push(34); 
+    for (int i = 0; i < 5; i++) {
+        cout << heap.top() << endl; 
+        heap.pop(); 
+    }
+    
     cout << heap.top() << endl; 
     heap.pop(); 
     cout << heap.top() << endl; 
@@ -33,9 +57,6 @@ int main() {
     heap.pop(); 
     cout << heap.top() << endl; 
     heap.pop(); 
-    cout << heap.top() << endl; 
-    heap.pop(); 
-    cout << heap.top() << endl; 
-    heap.pop(); 
-    cout << heap.top() << endl; 
+    cout << heap.top() << endl;
 }
+
